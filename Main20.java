f2db084d5f2137353f67f165e807a1c015102c61
Lime Icing <emil577b@stud.kea.dk>
2018-02-15 22:01:47 +0100

    Assignment prepped

---
+++ Main20.java
@@ -0,0 +1,11 @@
+public class Main20 {
+
+    /*
+    Write a modified version of the Vocabulary program developed in Chapter 10 that uses sets rather than ArrayList s to
+    store its words. (The program will be noticeably shorter and will run faster!)
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
